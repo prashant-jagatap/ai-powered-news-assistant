fa3cc3ca98cc0b2d4a5b9213d2e593f6e015ffdf
prashant-jagatap <prashant.jagatap@outlook.com>
2025-07-29 02:45:10 +0900

    sample main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Entry point for local testing of Alexa News Briefing skill
+"""
+
+from src.alexa_news import NewsHandler
+
+def main():
+    """Local CLI simulator for testing"""
+    print("Alexa News Briefing - Local Test Mode")
+    # TODO: Add CLI interface for testing intents
+    
+if __name__ == "__main__":
+    main()
